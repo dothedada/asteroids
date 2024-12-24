e141cab21a0f48ed5e223ed3d86c30dcaec1b03a
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-24 16:00:13 -0500

    Create constats.py and call it from main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE
 
 
 def main():
     print('Starting asteroids!')
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
 
 if __name__ == "__main__":
