ee2dd4a1f281cf8e92acbca63837412a3f15f267
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-24 16:11:53 -0500

    Changes formatter to Black

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE
+from constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_SPAWN_RATE,
+)
 
 
 def main():
-    print('Starting asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
