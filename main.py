400554aa960b1f591f7aa6b05fd7d91dbcc567dd
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-25 19:29:50 -0500

    Creates the container for the ship

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from constants import (
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0  # delta time
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,6 +21,25 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+class CircleShapr(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
 
 
 if __name__ == "__main__":
