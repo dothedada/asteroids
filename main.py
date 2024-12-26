08907b93876e980948979f28a301d7e396a38363
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-25 21:20:30 -0500

    Adds instances as groups in the gameloop

--- main.py
+++ main.py
@@ -9,22 +9,31 @@ from constants import (
 )
 from player import Player
 
-player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0  # delta time
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+
+        for sprite in updatable:
+            sprite.update(dt)
+
+        for sprite in updatable:
+            sprite.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
